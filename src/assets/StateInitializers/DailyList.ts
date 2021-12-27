da982ed977122eae2be04986620c5c1722c9eef2
srinathv31 <48702600+srinathv31@users.noreply.github.com>
2021-12-26 20:02:50 -0500

    Delete src/assets/StateInitializers directory

--- src/assets/StateInitializers/DailyList.ts
+++
@@ -1,9 +0,0 @@
-import Supplement from "../../interfaces/Supplement";
-import SupplementList from "../../assets/SupplementList.json";
-
-const DailyList: Record<string, Supplement> = 
-{
-    "12/23": SupplementList[0]
-}
-
-export default DailyList;
