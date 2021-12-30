80935763cc0d55b8d8369b2caf9dcff97ada7d86
Srinath Venkatesh <srinathvb1@gmail.com>
2021-12-30 00:48:51 -0500

    12/30 automatic random supplement selected in explore window

--- src/components/HomePage/ExploreWindow.tsx
+++ src/components/HomePage/ExploreWindow.tsx
@@ -1,8 +1,7 @@
 // Source Imports
-import React, { useEffect, useRef, useState } from "react";
-import { Animated, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
 import SupplementList from "../../assets/SupplementList.json";
-import * as Progress from "react-native-progress";
 
 // Component Imports
 
@@ -10,33 +9,11 @@ import * as Progress from "react-native-progress";
 
 export default function ExploreWindow(): JSX.Element {
 	const [randomSupplement, setRandomSupplement] = useState<number>(0);
-	const [x, setX] = useState<number>(0);
-
-	let progress = 0;
 
 	useEffect(() => {
 		const interval = setInterval(() => {
 			grabRandomSupplement();
-			console.log("random");
-			if (progress === 0) {
-				progress = 1;
-			}
-			else if (progress === 1) {
-				progress = 0;
-			}
-			setX(progress);
-			console.log(progress);
 		}, 5000);
-
-
-		// const timer = setInterval(() => {
-		// 	if (progress > 0.40) {
-		// 		progress = 0;
-		// 	}
-		// 	progress += 0.01;
-		// 	setX(progress);
-		// 	console.log(progress);
-		// }, 100);
 	
 		return () => (clearInterval(interval)); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
 	}, []);
@@ -52,7 +29,6 @@ export default function ExploreWindow(): JSX.Element {
 			<Text style={{ fontSize: 22, fontWeight: "600",  }}>Explore</Text>
 			<Text>{SupplementList[randomSupplement].name}</Text>
 			<Text>{SupplementList[randomSupplement].description}</Text>
-			<Progress.Bar animated progress={x} width={200} animationType="spring" height={100} style={{ zIndex: -10 }}/>
 		</View>
 	);
 }
