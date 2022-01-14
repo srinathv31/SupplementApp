1ea7781f271ac0a53abf32a3b3c053e760ccc546
Srinath Venkatesh <srinathvb1@gmail.com>
2022-01-14 02:59:15 -0500

    1/14 Breathing animated landing page

--- src/screens/WelcomePage.tsx
+++ src/screens/WelcomePage.tsx
@@ -1,45 +1,65 @@
 // Source Imports
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, Text, Animated } from "react-native";
 import Divider from "../components/Design/Divider";
 import { AppProps } from "../interfaces/Props";
 
 export default function WelcomePage({ userData, setPrevIndex }: AppProps): JSX.Element {
+    const [fadeStatus, setFadeStatus] = useState<boolean>(false);
+
     useEffect(() => {
         fadeIn();
-        fadeOut();
         fadeTop();
     }, []);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fadeStatus === true ? (startFadeOut(), setFadeStatus(false)) : (startFadeIn(), setFadeStatus(true));
+        }, 1750);
+	
+        return () => (clearInterval(interval));
+    }, [fadeStatus]);
+
     const fadeAnim = useRef(new Animated.Value(0)).current;
-    const fadeAnimBottom = useRef(new Animated.ValueXY()).current;
+    const fadeAnimSub = useRef(new Animated.Value(0)).current;
     const fadeAnimTop = useRef(new Animated.ValueXY()).current;
     fadeAnimTop.setOffset({ x: 0, y: -100 });
-    fadeAnimBottom.setOffset({ x: 0, y: 100 });
 
     const fadeIn = () => {
-        // Will change fadeAnim value to 1 in 5 seconds
+        // Will change fadeAnim value to 1 in 1 second
         Animated.timing(fadeAnim, {
             toValue: 1,
             duration: 1000,
             useNativeDriver: false
         }).start();
     };
-    const fadeOut = () => {
-        // Will change fadeAnim value to 0 in 3 seconds
-        Animated.timing(fadeAnimBottom, {
-            toValue: { x: 0, y: -100 },
-            duration: 1200,
-            useNativeDriver: false
-        }).start();
-    };
     const fadeTop = () => {
-        // Will change fadeAnim value to 0 in 3 seconds
+        // Brings Name from Top
         Animated.timing(fadeAnimTop, {
             toValue: { x: 0, y: 100 },
             duration: 1200,
             useNativeDriver: false
         }).start();
+    };
+
+    const startFadeIn = () => {
+        // Breathe In
+        Animated.timing(fadeAnimSub, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: false
+        }).start();
+        setFadeStatus(true);
+    };
+
+    const startFadeOut = () => {
+        // Breathe In
+        Animated.timing(fadeAnimSub, {
+            toValue: 0.3,
+            duration: 1000,
+            useNativeDriver: false
+        }).start();
+        setFadeStatus(!fadeStatus);
     };
 
     return(
@@ -48,7 +68,7 @@ export default function WelcomePage({ userData, setPrevIndex }: AppProps): JSX.E
                 <Text style={{ color: "white", fontSize: 28, textAlign: "center", padding: 10 }}>Welcome {userData.name}</Text>
             </Animated.View>
             <Divider length="small"></Divider>
-            <Animated.View style={[{ opacity: fadeAnim }, fadeAnimBottom.getLayout()]}>
+            <Animated.View style={[{ opacity: fadeAnimSub }]}>
                 <Text onPress={() => setPrevIndex(1)} style={{ color: "white", fontSize: 23, textAlign: "center", padding: 10 }}>Enter</Text>
             </Animated.View>
         </View>
