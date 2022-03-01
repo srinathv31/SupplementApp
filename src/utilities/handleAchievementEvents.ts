4b5c0c24ff91c4d4b790f0362307b46ad6112ec3
Srinath Venkatesh <srinathvb1@gmail.com>
2022-03-01 15:31:42 -0500

    3/1 added journal achievement

--- src/utilities/handleAchievementEvents.ts
+++ src/utilities/handleAchievementEvents.ts
@@ -2,17 +2,15 @@ import { Achievement } from "../interfaces/Achievements";
 import { AppProps } from "../interfaces/Props";
 import { showAchievementToast } from "./toasts";
 
-export function achievementUnlocked({ completedAchievements, setCompletedAchievements, setModalVisible }: {
-    setCompletedAchievements: AppProps["setCompletedAchievements"], completedAchievements: AppProps["completedAchievements"],
-    setModalVisible: AppProps["setModalVisible"]
-}) {
+export function achievementUnlocked(completedAchievements: AppProps["completedAchievements"], setCompletedAchievements: AppProps["setCompletedAchievements"], 
+    setModalVisible: AppProps["setModalVisible"], index: number) {
     const completedAchievementsCopy: Achievement[] = [];
 
     completedAchievements.forEach(item => {
         completedAchievementsCopy.push(item);
     });
 
-    completedAchievementsCopy[0].color === "white" ? completedAchievementsCopy[0].color = "skyblue" : completedAchievementsCopy[0].color = "white";
-    showAchievementToast(completedAchievementsCopy[0], setModalVisible);
+    completedAchievementsCopy[0].color === "white" ? completedAchievementsCopy[index].color = "skyblue" : completedAchievementsCopy[index].color = "white";
+    setTimeout(() => showAchievementToast(completedAchievementsCopy[index], setModalVisible), 1000);
     setCompletedAchievements(completedAchievementsCopy);
 }
