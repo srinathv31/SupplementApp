59ebebd4c71ea2b8a573916b61119ea000ef1d8e
Srinath Venkatesh <srinathvb1@gmail.com>
2022-03-14 22:35:46 -0400

    3/14 imgur hosting + revamped onboardinng

--- src/screens/OnboardingTour.tsx
+++ src/screens/OnboardingTour.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Image } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
-import { penguinPic } from "../assets/imageURLs/profilePictureURLs";
+import { addBatchPic, addButtonPic, calendarPic, dailyPic, feedbackGIF, introPic, LogoPic, moodPic, researchPic, sharePic } from "../assets/imageURLs/onboardingURLs";
 import { AppProps } from "../interfaces/Props";
 
 export default function OnboardingTour({ setPage }: {
@@ -15,22 +15,65 @@ export default function OnboardingTour({ setPage }: {
             showSkip={false}
             pages={[
                 {
-                    backgroundColor: "#fff",
-                    image: <Image source={{ uri: penguinPic }} style={{ height: 100, width: 100 }} />,
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: introPic }} style={{ height: 300, width: 300 }} />,
                     title: "Welcome to Your New Supplement & Lifestyle Planner",
                     subtitle: "We're excited for you to start your better health journey ❤️",
                 },
                 {
-                    backgroundColor: "#fe6e58",
-                    image: <Image source={{ uri: penguinPic }} style={{ height: 100, width: 100 }} />,
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: addButtonPic }} style={{ height: 200, width: 375 }} />,
                     title: "Start by Scheduling a Supplement 💪",
-                    subtitle: "This is the subtitle that sumplements the title.",
+                    subtitle: "It's as easy as clicking this button and then choosing a supplement.",
                 },
                 {
-                    backgroundColor: "skyblue",
-                    image: <Image source={{ uri: penguinPic }} style={{ height: 100, width: 100 }} />,
-                    title: "Get Information on New Supplements 🔍",
-                    subtitle: "Beautiful, isn't it?",
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: addBatchPic }} style={{ height: 200, width: 375 }} />,
+                    title: "Or Schedule a Bunch of Supplements at Once",
+                    subtitle: "You can select all the days you want to add a supplement to. Just to make scheduling easier 😉",
+                },
+                {
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: researchPic }} style={{ height: 215, width: 375 }} />,
+                    title: "Start your Research on Supplement Now",
+                    subtitle: "Tap any Supplement Card to Get Up-To-Date Research, Recomended Dosage, and more 🔍",
+                },
+                {
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: dailyPic }} style={{ height: 200, width: 375 }} />,
+                    title: "Get Specific as You Want",
+                    subtitle: "Tap a Supplement Card in your schedule to get a detailed breakdown.",
+                },
+                {
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: moodPic }} style={{ height: 200, width: 375 }} />,
+                    title: "Track your Mood 😄",
+                    subtitle: "Follow your mental health trends.",
+                },
+                {
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: calendarPic }} style={{ height: 300, width: 310 }} />,
+                    title: "Track your Days on the Calendar and in a Daily Journal 📆",
+                    subtitle: "On the Calendar Page, an Orange Dot 🟠 indicates a Supplement and a Green Dot 🟢 indicates a Journal Entry.",
+                    
+                },
+                {
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: sharePic }} style={{ height: 200, width: 375 }} />,
+                    title: "Share a New Supplement You Found or Your Daily Schedule ✈️",
+                    subtitle: "The share button will automatically appear on pages you can share on 🪄.",
+                },
+                {
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: feedbackGIF }} style={{ height: 200, width: 375 }} />,
+                    title: "Beta",
+                    subtitle: "Cool",
+                },
+                {
+                    backgroundColor: "#0B172A",
+                    image: <Image source={{ uri: LogoPic }} style={{ height: 200, width: 375 }} />,
+                    title: "Now Let's Go Build a Healthier Lifestyle 😉",
+                    subtitle: "",
                 },
             ]}
         />
