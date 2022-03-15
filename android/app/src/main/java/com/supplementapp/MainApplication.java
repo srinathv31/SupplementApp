74b938fb27f797e68e33314028082f55ba032cbd
Srinath Venkatesh <srinathvb1@gmail.com>
2022-03-14 23:45:53 -0400

    3/14 android build fix

--- android/app/src/main/java/com/supplementapp/MainApplication.java
+++ android/app/src/main/java/com/supplementapp/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.rnfs.RNFSPackage;
+import com.brentvatne.react.ReactVideoPackage;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -24,6 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          packages.add(new ReactVideoPackage());
+          packages.add(new RNFSPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
