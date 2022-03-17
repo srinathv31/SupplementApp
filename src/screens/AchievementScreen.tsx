880caf573fd309a19b6d2de013cde8ce8edc9e93
Srinath Venkatesh <srinathvb1@gmail.com>
2022-03-16 22:30:20 -0400

    3/16 achievement screen ui fix

--- src/screens/AchievementScreen.tsx
+++ src/screens/AchievementScreen.tsx
@@ -65,8 +65,6 @@ const styles = StyleSheet.create({
     modalView: {
         width: "100%", padding: 10,
         height: "95%",
-        borderRadius: 10,
-        borderWidth: 1,
         backgroundColor: "#0B172A",
         shadowColor: "#000",
         shadowOffset: {
