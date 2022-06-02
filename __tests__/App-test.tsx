6b19b121551b36ad49502f6cc60e8f7e0fb80884
Srinath Venkatesh <srinathv@udel.edu>
2022-06-02 18:22:08 -0400

    test updated

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,6 +9,10 @@ import "react-native";
 // // Note: test renderer must be required after react-native.
 // import renderer from "react-test-renderer";
 import { generateCurrentDateObject, generateWeekList } from "../src/utilities/getCurrentDate";
+import { randomDateGenerator } from "../src/utilities/testingFunctions/randomDateGenerator";
+import SupplementList from "../src/assets/SupplementList.json";
+import { SupplementObject } from "../src/interfaces/Supplement";
+import { convertStringTimeToDateTime } from "../src/utilities/convertTime";
 
 // it("renders correctly", () => {
 // 	renderer.create(<App />);
@@ -20,3 +24,19 @@ it("Creates a week", () => {
     expect(week.length === 7);
 });
 
+it("Creates a Date Object from the given Date String 50 times", () => {
+    const correctDateFormatList = [];
+    const randomDates = randomDateGenerator();
+    const testSupplement: SupplementObject = {
+        Supplement: SupplementList[0],
+        time: "07:30 PM",
+        taken: "not-taken"
+    };
+    randomDates.forEach((item) => {
+        const generatedDateObject = convertStringTimeToDateTime(testSupplement, item);
+        if (isNaN(generatedDateObject.getMonth()) === false){
+            correctDateFormatList.push(generatedDateObject);
+        }
+    });
+    expect(correctDateFormatList.length).toEqual(50);
+});
