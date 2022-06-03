033fc5be10a951bb17ca4bfc839a0826fe0988ed
Srinath Venkatesh <srinathv@udel.edu>
2022-06-02 22:18:57 -0400

    timelineobject generator implemented

--- src/components/Mood/MoodTimelinePicker.tsx
+++ src/components/Mood/MoodTimelinePicker.tsx
@@ -8,6 +8,7 @@ import MoodObject from "../../interfaces/Mood";
 import { SupplementMapObject } from "../../interfaces/Supplement";
 import { achievementUnlocked } from "../../utilities/handleAchievementEvents";
 import { allPropsContext } from "../../contextHooks/AllPropsContext";
+import { generateTimelineObject } from "../../utilities/generateTimelineObject";
 
 export default function MoodTimelinePicker(): JSX.Element {
     const { modalVisible, setSupplementMap, supplementMap, daySelected, setModalVisible, completedAchievements, setCompletedAchievements } = useContext(allPropsContext);
@@ -18,44 +19,10 @@ export default function MoodTimelinePicker(): JSX.Element {
     const [startSelected, setStartSelected] = useState<boolean>(false);
     const [currentKey, setCurrentKey] = useState<string>("1");
 
-    const data: TimeLineObject[] = [
-        { time: "12:00 A" },
-        { time: "01:00 A" },
-        { time: "02:00 A" },
-        { time: "03:00 A" },
-        { time: "04:00 A" },
-        { time: "05:00 A" },
-        { time: "06:00 A" },
-        { time: "07:00 A" },
-        { time: "08:00 A" },
-        { time: "09:00 A" },
-        { time: "10:00 A" },
-        { time: "11:00 A" },
-        { time: "12:00 P" },
-        { time: "01:00 P" },
-        { time: "02:00 P" },
-        { time: "03:00 P" },
-        { time: "04:00 P" },
-        { time: "05:00 P" },
-        { time: "06:00 P" },
-        { time: "07:00 P" },
-        { time: "08:00 P" },
-        { time: "09:00 P" },
-        { time: "10:00 P" },
-        { time: "11:00 P" },
-        { time: "12:00 A" },
-        { time: "01:00 A" },
-        { time: "02:00 A" },
-        { time: "03:00 A" },
-        { time: "04:00 A" },
-        { time: "05:00 A" },
-        { time: "06:00 A" },
-        { time: "07:00 A" },
-    ];
-    const [timelineState, setTimelineState] = useState<TimeLineObject[]>(data);
+    const [timelineState, setTimelineState] = useState<TimeLineObject[]>(generateTimelineObject());
 
     useEffect(() => {
-        const timelineStateCopy = data;
+        const timelineStateCopy = generateTimelineObject();
         setTimelineState(timelineStateCopy);
     }, [modalVisible]);
 
@@ -137,7 +104,7 @@ export default function MoodTimelinePicker(): JSX.Element {
 
     const lastMood: MoodObject = supplementMap[daySelected] !== undefined && supplementMap[daySelected].DailyMood !== undefined ? 
         supplementMap[daySelected].DailyMood[currentKey] :
-        { mood: "", range: 0 , TimelineData: data };
+        { mood: "", range: 0 , TimelineData: generateTimelineObject() };
 
     return(
         <>
--- src/components/Mood/MoodTimlineSupplement.tsx
+++ src/components/Mood/MoodTimlineSupplement.tsx
@@ -6,6 +6,7 @@ import IconI from "react-native-vector-icons/Ionicons";
 import { TimeLineObject } from "../../interfaces/TimeLine";
 import MoodObject from "../../interfaces/Mood";
 import { allPropsContext } from "../../contextHooks/AllPropsContext";
+import { generateTimelineObject } from "../../utilities/generateTimelineObject";
 
 export default function MoodTimlineSupplement({ timelineData }: {
     timelineData: MoodObject
@@ -25,44 +26,10 @@ export default function MoodTimlineSupplement({ timelineData }: {
         { id: 5, name: "radio-button-off-outline" }
     ];
 
-    const data: TimeLineObject[] = [
-        { time: "12:00 A" },
-        { time: "01:00 A" },
-        { time: "02:00 A" },
-        { time: "03:00 A" },
-        { time: "04:00 A" },
-        { time: "05:00 A" },
-        { time: "06:00 A" },
-        { time: "07:00 A" },
-        { time: "08:00 A" },
-        { time: "09:00 A" },
-        { time: "10:00 A" },
-        { time: "11:00 A" },
-        { time: "12:00 P" },
-        { time: "01:00 P" },
-        { time: "02:00 P" },
-        { time: "03:00 P" },
-        { time: "04:00 P" },
-        { time: "05:00 P" },
-        { time: "06:00 P" },
-        { time: "07:00 P" },
-        { time: "08:00 P" },
-        { time: "09:00 P" },
-        { time: "10:00 P" },
-        { time: "11:00 P" },
-        { time: "12:00 A" },
-        { time: "01:00 A" },
-        { time: "02:00 A" },
-        { time: "03:00 A" },
-        { time: "04:00 A" },
-        { time: "05:00 A" },
-        { time: "06:00 A" },
-        { time: "07:00 A" },
-    ];
     const [timelineState, setTimelineState] = useState<TimeLineObject[]>(
         timelineData.TimelineData !== [] ? 
         timelineData.TimelineData as TimeLineObject[] :
-            data
+            generateTimelineObject()
     );
 
     const MoodTimelineProps = {
