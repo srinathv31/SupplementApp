abeb27a2ee4ef9cfd240afc4fcc1765592f12e2e
Srinath Venkatesh <srinathvb1@gmail.com>
2023-03-15 19:18:47 -0400

    graph start

--- src/components/Mood/MoodAnalysis.tsx
+++ src/components/Mood/MoodAnalysis.tsx
@@ -1,23 +1,23 @@
 // Source Imports
-// import { BottomTabBar } from "./BottomTabBar";
-import React from "react";
+import { BottomTabBar } from "./BottomTabBar";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-// import MoodBarGraph from "./MoodBarGraph";
+import MoodBarGraph from "./MoodBarGraph";
 import useClientStore from "../../zustand/clientStore";
 
 export default function MoodAnalysis(): JSX.Element {
     const week = useClientStore(state => state.week);
 
-    // const [graphType, setGraphType] = useState<string>("data0");
-    // const [tabSelect, setTabSelect] = useState<"weekly" | "daily" | "monthly">("weekly");
+    const [graphType, setGraphType] = useState<string>("data0");
+    const [tabSelect, setTabSelect] = useState<"weekly" | "daily" | "monthly">("weekly");
 
     return(
         <View style={{ flex: 1, flexDirection: "column" }}>
             <Text style={styles.modalText}>{week[0].dateString+""} - {week[6].dateString+""}</Text>
-            {/* <View style={{ flex: 1, flexDirection: "column", justifyContent: "space-between" }}>
+            <View style={{ flex: 1, flexDirection: "column", justifyContent: "space-between" }}>
                 <MoodBarGraph graphType={graphType} />
-                <BottomTabBar setTabSelect={setTabSelect} setGraphType={setGraphType} tabSelect={tabSelect}  />
-            </View> */}
+                {/* <BottomTabBar setTabSelect={setTabSelect} setGraphType={setGraphType} tabSelect={tabSelect}  /> */}
+            </View>
         </View>
     );
 }
